f621c5a05a75329b6559f77dc94796ade1396128
Chris Thi <chris.c.thi@gmail.com>
2015-05-31 19:56:48 -0400

    switch to design lib drawer

--- lolstorm/src/main/java/com/jclolstorm/lolstorm/ui/HomeActivity.java
+++ lolstorm/src/main/java/com/jclolstorm/lolstorm/ui/HomeActivity.java
@@ -1,25 +1,27 @@
 package com.jclolstorm.lolstorm.ui;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.jclolstorm.lolstorm.R;
-import com.jclolstorm.lolstorm.adapters.HomePagerAdapter;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
-public class HomeActivity extends AppCompatActivity{
+public class HomeActivity extends AppCompatActivity {
 
     @InjectView(R.id.home_activity_toolbar)
     Toolbar mToolbar;
-    @InjectView(R.id.home_activity_tab_host)
-    TabLayout mTabLayout;
-    @InjectView(R.id.home_activity_viewpager)
-    ViewPager mViewPager;
+    @InjectView(R.id.home_drawer_layout)
+    DrawerLayout mDrawerLayout;
+    @InjectView(R.id.home_drawer)
+    NavigationView mNavView;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,20 +30,52 @@ public class HomeActivity extends AppCompatActivity{
         ButterKnife.inject(this);
 
         initToolbar();
-        initPager();
+        initNavDrawer();
+        linkDrawer();
+    }
+
+    @Override
+    public void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
     private void initToolbar() {
         if (null != mToolbar) {
             setSupportActionBar(mToolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
     }
 
-    private void initPager() {
-        if (null != mTabLayout) {
-            mViewPager.setAdapter(new HomePagerAdapter(this, getSupportFragmentManager()));
+    private void initNavDrawer() {
+        if (null != mNavView) {
+            mNavView.setNavigationItemSelectedListener((menuItem -> {
+                menuItem.setChecked(true);
 
-            mTabLayout.setupWithViewPager(mViewPager);
+                // TODO fix this
+                String item = menuItem.getTitle().toString();
+                if(item.equals("Search")) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.home_fragment, SummonerSearchFragment.newInstance())
+                            .commit();
+                }
+                mDrawerLayout.closeDrawers();
+                return true;
+            }));
+
         }
+    }
+
+    private void linkDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.drawer_open, R.string.drawer_close);
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
 }
