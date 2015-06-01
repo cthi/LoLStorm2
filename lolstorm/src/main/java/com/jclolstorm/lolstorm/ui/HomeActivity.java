d1ec183f530582bc7cc40ae71fe3bd647c579c02
Chris Thi <chris.c.thi@gmail.com>
2015-05-31 20:10:19 -0400

    fix cardview color and delay ft in drawer

--- lolstorm/src/main/java/com/jclolstorm/lolstorm/ui/HomeActivity.java
+++ lolstorm/src/main/java/com/jclolstorm/lolstorm/ui/HomeActivity.java
@@ -2,6 +2,7 @@ package com.jclolstorm.lolstorm.ui;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -60,13 +61,20 @@ public class HomeActivity extends AppCompatActivity {
 
                 // TODO fix this
                 String item = menuItem.getTitle().toString();
-                if(item.equals("Search")) {
-                    getSupportFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.home_fragment, SummonerSearchFragment.newInstance())
-                            .commit();
-                }
                 mDrawerLayout.closeDrawers();
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(item.equals("Search")) {
+                            getSupportFragmentManager()
+                                    .beginTransaction()
+                                    .replace(R.id.home_fragment, SummonerSearchFragment.newInstance())
+                                    .commit();
+                        }
+
+                    }
+                }, 300);
                 return true;
             }));
 
