d6b6fb7896581a443d8bdb2c9a74065cc5de84f4
Chris Thi <chris.c.thi@gmail.com>
2015-06-02 21:43:30 -0400

    change theme color

--- lolstorm/src/main/java/com/jclolstorm/lolstorm/presenters/SummonerChampionStatsPresenter.java
+++ lolstorm/src/main/java/com/jclolstorm/lolstorm/presenters/SummonerChampionStatsPresenter.java
@@ -29,26 +29,26 @@ public class SummonerChampionStatsPresenter {
     private void processStats(AggregatedStats stats) {
         ArrayList<Stat> statList = new ArrayList<>();
 
-        statList.add(new Stat("Games Played", stats.getTotalSessionsPlayed(), R.color.blue));
-        statList.add(new Stat("Games Won", stats.getTotalSessionsWon(), R.color.blue));
-        statList.add(new Stat("Games Lost", stats.getTotalSessionsLost(), R.color.blue));
-        statList.add(new Stat("Gold Earned", stats.getTotalGoldEarned(), R.color.blue));
-        statList.add(new Stat("Kills", stats.getTotalChampionKills(), R.color.blue));
-        statList.add(new Stat("Deaths", stats.getTotalDeathsPerSession(), R.color.blue));
-        statList.add(new Stat("Assists", stats.getTotalAssists(), R.color.blue));
-        statList.add(new Stat("Max Kills", stats.getMostChampionKillsPerSession(), R.color.blue));
-        statList.add(new Stat("Damage Dealt", stats.getTotalDamageDealt(), R.color.blue));
-        statList.add(new Stat("Physical Damage", stats.getTotalPhysicalDamageDealt(), R.color.blue));
-        statList.add(new Stat("Magic Damage Dealt", stats.getTotalMagicDamageDealt(), R.color.blue));
-        statList.add(new Stat("Damage Taken", stats.getTotalDamageTaken(), R.color.blue));
-        statList.add(new Stat("Double Kills", stats.getTotalDoubleKills(), R.color.blue));
-        statList.add(new Stat("Triple Kills", stats.getTotalTripleKills(), R.color.blue));
-        statList.add(new Stat("Quadra Kills", stats.getTotalQuadraKills(), R.color.blue));
-        statList.add(new Stat("Penta Kills", stats.getTotalPentaKills(), R.color.blue));
-        statList.add(new Stat("Legendary Kills", stats.getTotalUnrealKills(), R.color.blue));
-        statList.add(new Stat("Minions Killed", stats.getTotalMinionKills(), R.color.blue));
-        statList.add(new Stat("Jungle Minions Killed", stats.getTotalNeutralMinionsKilled(), R.color.blue));
-        statList.add(new Stat("Turrets Destroyed", stats.getTotalTurretsKilled(), R.color.blue));
+        statList.add(new Stat("Games Played", stats.getTotalSessionsPlayed(), R.color.green_primary));
+        statList.add(new Stat("Games Won", stats.getTotalSessionsWon(), R.color.green_primary));
+        statList.add(new Stat("Games Lost", stats.getTotalSessionsLost(), R.color.green_primary));
+        statList.add(new Stat("Gold Earned", stats.getTotalGoldEarned(), R.color.green_primary));
+        statList.add(new Stat("Kills", stats.getTotalChampionKills(), R.color.green_primary));
+        statList.add(new Stat("Deaths", stats.getTotalDeathsPerSession(), R.color.green_primary));
+        statList.add(new Stat("Assists", stats.getTotalAssists(), R.color.green_primary));
+        statList.add(new Stat("Max Kills", stats.getMostChampionKillsPerSession(), R.color.green_primary));
+        statList.add(new Stat("Damage Dealt", stats.getTotalDamageDealt(), R.color.green_primary));
+        statList.add(new Stat("Physical Damage", stats.getTotalPhysicalDamageDealt(), R.color.green_primary));
+        statList.add(new Stat("Magic Damage Dealt", stats.getTotalMagicDamageDealt(), R.color.green_primary));
+        statList.add(new Stat("Damage Taken", stats.getTotalDamageTaken(), R.color.green_primary));
+        statList.add(new Stat("Double Kills", stats.getTotalDoubleKills(), R.color.green_primary));
+        statList.add(new Stat("Triple Kills", stats.getTotalTripleKills(), R.color.green_primary));
+        statList.add(new Stat("Quadra Kills", stats.getTotalQuadraKills(), R.color.green_primary));
+        statList.add(new Stat("Penta Kills", stats.getTotalPentaKills(), R.color.green_primary));
+        statList.add(new Stat("Legendary Kills", stats.getTotalUnrealKills(), R.color.green_primary));
+        statList.add(new Stat("Minions Killed", stats.getTotalMinionKills(), R.color.green_primary));
+        statList.add(new Stat("Jungle Minions Killed", stats.getTotalNeutralMinionsKilled(), R.color.green_primary));
+        statList.add(new Stat("Turrets Destroyed", stats.getTotalTurretsKilled(), R.color.green_primary));
 
         view.populateAdapter(statList);
     }
